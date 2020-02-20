1b65e5b11e747f4eaf15f9728e6ed099d0dfd47f
Preshailen Ruthnam <preshalienr@morecorp.co.za>
2020-02-20 11:07:41 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+
+import { SidebarModule } from './sidebar/sidebar.module';
+import { NavbarModule} from './shared/navbar/navbar.module';
+
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot(AppRoutes, {
+      useHash: true
+    }),
+    SidebarModule,
+    NavbarModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
